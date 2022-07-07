9b093f07e20ac53a41d67d8ee42e612006c587bf
Roman Dimov <romanddimov@gmail.com>
2022-07-06 17:12:03 -0700

    add honeycomb lattice builder, coordinate list from lattice

--- src/graphs.jl
+++ src/graphs.jl
@@ -20,3 +20,25 @@ function build_lattice(graph::Graph)::Lattice
     es = edges(graph)
     latt = map(edge -> LatticeBond(src(edge), dst(edge)), es)
 end
+
+"""
+Builds a dictionary mapping the site index to cartesian coordinates
+"""
+function get_coords(lattice::Lattice)
+    # make index => (x, y) pairs
+    getcoords(bond) = [bond.s1 => (bond.x1, bond.y1), bond.s2 => (bond.x2, bond.y2)]
+    allcoords = vcat(getcoords.(lattice)...)
+
+    # create dictionary to remove duplicates
+    dict = Dict(allcoords)
+
+    # create array or coordinates indexed by the site index
+    indices = keys(dict)
+    maxidx = maximum(indices)
+    coordlist = Vector(undef, maxidx)
+    for (idx, coords) in dict
+        coordlist[idx] = coords
+    end
+
+    return coordlist
+end
--- src/lattices.jl
+++ src/lattices.jl
@@ -38,6 +38,57 @@ function build_square_lattice(nx::Int, ny::Int; yperiodic::Bool=false, neighbor:
     end
 end
 
+"""
+Build honeycomb lattice in armchair configuration
+
+y = 3 o - o - o - o - o - o - o - o
+        /      \\       /       \
+y = 2 o - o - o - o - o - o - o - o
+        /      \\       /       \
+y = 1 o - o - o - o - o - o - o - o
+  x = 1   2   3   4   5   6   7   8
+"""
 function build_honeycomb_lattice(nx::Int, ny::Int; yperiodic::Bool=false, neighbor::Int=1)::Lattice
-    error("Not implemented!")
+    # only handle nearest neighbor
+    @assert neighbor == 1 "Only handles neighbor = 1"
+
+    getidx(x, y) = (x - 1) * ny + y
+    wrap(x, k) = ((x % k) + k) % k
+
+    function getcoords(x, y)
+        xsteps = [0, 1/2, 3/2]
+        xcoord = ((x - 1) ÷ 3) * 2 + xsteps[mod1(x, 3)]
+        ycoord = y - 1 + (mod1(x, 3) == 1 ? sqrt(3) / 2 : 0)
+        return xcoord, ycoord
+    end
+
+    lattice = Lattice(undef, 0)
+
+    function bond!(x1, y1, x2, y2)
+        idx1 = getidx(x1, y1)
+        idx2 = getidx(x2, y2)
+        x1coord, y1coord = getcoords(x1, x2)
+        x2coord, y2coord = getcoords(x2, y2)
+        push!(lattice, LatticeBond(idx1, idx2, x1coord, y1coord, x2coord, y2coord))
+    end
+
+    for x in 1:(nx - 1)
+        for y in 1:ny
+            # bind to next row
+            bond!(x, y, x + 1, y)
+
+            # Alternate between linking up and linking down on odd rows
+            if x % 2 == 1
+                # alternate between bonding up a leg or down a leg
+                crossy = y + ((x ÷ 2) % 2 == 0 ? 1 : -1)
+                if crossy ∈ 1:ny
+                    bond!(x, y, x + 1, crossy)
+                elseif yperiodic
+                    bond!(x, y, x + 1, wrap(crossy, ny))
+                end
+            end
+        end
+    end
+
+    return lattice
 end
